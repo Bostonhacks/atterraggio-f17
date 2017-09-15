8fc726af1047abf834f9aa633b55f0af1f52cf6b
Lucas <bigluc23@gmail.com>
2017-09-15 17:32:29 -0400

    Better organization

--- public/hamburger.js
+++
@@ -1,9 +0,0 @@
-$("#nav--burger").click(function(){
-    if ($('#nav--burger').hasClass('is-active')) {
-        $("#nav--burger").removeClass('is-active');
-        $('.navLink').css("display", "none");
-    } else {
-        $("#nav--burger").addClass(" is-active");
-        $('.navLink').css("display", "block");
-    }
-});
